3fe28cdfb80d227796210a4b3d872d789948ed0d
Diana <diana.dutuu@gmail.com>
2024-04-03 10:08:29 +0300

    afisare pe pagina cu thymeleaf

--- src/main/java/com/licenta/wireless/Controller/NetworkController.java
+++ src/main/java/com/licenta/wireless/Controller/NetworkController.java
@@ -2,26 +2,38 @@ package com.licenta.wireless.Controller;
 
 import com.licenta.wireless.Model.NetworkInfo;
 import com.licenta.wireless.util.HtmlParserUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @CrossOrigin
-@RestController
+@Controller
 public class NetworkController {
 
     private final HtmlParserUtil htmlParserUtil;
 
-    // Injectează HtmlParserUtil folosind constructorul
     public NetworkController(HtmlParserUtil htmlParserUtil) {
         this.htmlParserUtil = htmlParserUtil;
     }
 
+    // Servește JSON-ul
     @GetMapping("/networks")
+    @ResponseBody
     public List<NetworkInfo> getNetworkInfos() {
-        // Presupunem că metoda parseHtml este modificată pentru a fi accesibilă public sau printr-un serviciu
         return htmlParserUtil.parseHtml();
+    }
+
+    // Servește pagina HTML
+    @GetMapping("/network-page")
+    public String showNetworksPage(Model model) {
+        List<NetworkInfo> networkInfos = htmlParserUtil.parseHtml();
+        model.addAttribute("networks", networkInfos);
+        return "networks"; // Numele template-ului Thymeleaf
     }
 }
